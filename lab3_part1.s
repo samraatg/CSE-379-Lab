bc5b370b76643b9b4de0ff204bebfff3b038138e
AviUB <47643721+AviUB@users.noreply.github.com>
2021-03-02 21:02:05 -0500

    read_character and output_character fixed

--- lab3_part1.s
+++ lab3_part1.s
@@ -7,10 +7,10 @@ lab3:
 	STMFD SP!,{lr}	; Store register lr on stack
 
 		; Your code is placed here
-		MOV r0, #0x31 ; pass ASCII "1" to r0 to test output_character
-		BL output_character
-		MOV r0, #0x30 ; changing r0 for testing purposes
 		BL read_character
+		;MOV r0, #0x31 ; pass ASCII "1" to r0 to test output_character
+		BL output_character
+		;BL read_character
 	LDMFD sp!, {lr}
 	mov pc, lr
 
@@ -23,10 +23,11 @@ output_character:
 
 		MOV r1, #0xC000
 		MOVT r1, #0x4000 ; r1 = base address of UART
-		LDRB r2, [r1, #U0FR] ; load U0FR into r2
 
-		AND r2, r2, #0x20 ; mask all bits except TxFF (bit 5)
 TEST_TFF:
+		LDRB r2, [r1, #U0FR] ; load 0x4000C018 (U0FR) into r2
+		AND r2, r2, #0x20 ; mask all bits except TxFF (bit 5)
+
 		CMP r2, #0x20	; Test TxFF in Status register
 		BEQ TEST_TFF ; if TxFF == 1, test again
 		STRB r0, [r1] ; else TxFF == 0, store byte (r0) in transmit register (r1)
@@ -42,10 +43,10 @@ read_character:
 
 		MOV r1, #0xC000
 		MOVT r1, #0x4000 ; r1 = base address of UART
-		LDRB r2, [r1, #U0FR] ; load U0FR into r2
-
-		AND r2, r2, #0x10 ; mask all bits except RxFE (bit 4)
 TEST_RFE:
+		LDRB r2, [r1, #U0FR] ; load 0x4000C018 (U0FR) into r2
+		AND r2, r2, #0x10 ; mask all bits except RxFE (bit 4)
+
 		CMP r2, #0x10	; Test RxFE in Status register
 		BEQ TEST_RFE ; if RxFE == 1, test again
 		LDRB r0, [r1] ; else RxFE == 0, read byte (r0) from receive register (r1)
