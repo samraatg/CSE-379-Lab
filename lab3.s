eb8485de46e35d8afdae0e09a1ee889bf6b8dc37
AviUB <47643721+AviUB@users.noreply.github.com>
2021-03-04 22:11:56 -0500

    All lab2 subroutines working with negatives

--- lab3.s
+++ lab3.s
@@ -9,10 +9,10 @@
 prompt1:	.string "Enter a number: ",0
 prompt2:	.string "Enter another number: ",0
 prompt3:	.string "Enter an operation (+ or -) : ",0
-results:	.string "-5678",0
+results:	.string "-999999",0
 num_1_string: .string "Place holder string for your first number",0
 num_2_string: .string "Place holder string for your second number",0
-prompt_end: .string "Do you want to enter another operation? Press enter: ",0
+prompt_end: .string "Do you want to enter another operation? (Y/N): ",0
 .text
 	.global lab3
 U0FR:  .equ 0x18			; UART0 Flag Register
@@ -38,6 +38,13 @@ lab3:
 		; r0 = results string
 		MOV r1, r4
 		BL str2int ;
+		MOV r1, r5
+		MOV r4, r0
+		BL num_digits
+		MOV r2, r0
+		MOV r0, r4
+		BL int2str
+		MOV r0, r0
 		;read_string prompt1
 		;output_string prompt1
 		;read_string first number
@@ -125,13 +132,18 @@ uart_init:
 ; output: length of integer in r0
 num_digits:
  	STMFD r13!, {r14}
+ 		CMP r0, #0 ; check if r0 is negative
+ 		BGE ND_START
+ 		MOV r9, #-1
+ 		MUL r0, r9 ; if so, make positive
+ND_START:
  		MOV r7, #0 ; initialize number of digits (n) to zero
-NDLOOP:
+ND_LOOP:
 		MOV r8, #10
 		UDIV r0, r0, r8 ; divide i by 10
 		ADD r7, r7, #1 ; add 1 to n
 		CMP r0, #0 ; branch if i != 0
-		BNE NDLOOP
+		BNE ND_LOOP
 		; else exit
 		MOV r0, r7 ; return n in r0
  	LDMFD r13!, {r14}
@@ -146,20 +158,20 @@ str2int:
 		LDRB r8, [r1] ; load first character to see if negative integer
 		MOV r9, #1
 		CMP r8, #0x2D ; compare character with ASCII '-'
-		BNE S2ILOOP
+		BNE S2I_LOOP
 		ADD r1, r1, #1 ; if first char is '-', move pointer to second char
 		MOV r9, #-1 ; r9 = -1 for multiplying later
-S2ILOOP:
+S2I_LOOP:
 		LDRB r8, [r1] ; load character from address pointed to by r1
 		CMP r8, #0 ; branch to S2IDONE if char == 0
-		BEQ S2IDONE
+		BEQ S2I_DONE
 		MOV r10, #10
 		MULT r7, r7, r10 ; multiply i by 10
 		SUB r8, r8, #0x30 ; subtract 0x30 (ASCII 0) from char to get digit value
 		ADD r7, r7, r8 ; add digit value to i
 		ADD r1, r1, #1 ; add one to pointer
-		B S2ILOOP ; jump back to load char step
-S2IDONE:
+		B S2I_LOOP ; jump back to load char step
+S2I_DONE:
 		MULT r7, r9
 		MOV r0, r7 ; return i in r0
 	LDMFD r13!, {r14}
@@ -170,23 +182,37 @@ S2IDONE:
 int2str:
 	STMFD r13!, {r14}
 	; Your code for the int2str routine goes here.
-		ADD r1,r1,r2    ;Add num of digits to pinter r1=r1+r2
-		MOV r8,#0 		;Store 0 at r8 to be used as NULL
-		STRB r8,[r1]	;Store Null at adress pointed to by pointer
+		ADD r1,r1,r2    ;Add num of digits to pointer r1=r1+r2
+		MOV r2, #0 ; placeholder for negative check
+		CMP r0, #0 ; check to see if integer is negative
+		BGE I2S_START
+		ADD r1, r1, #1 ;add another "digit" to account for '-'
+		MOV r2, #-1
+		MULT r0, r0, r2 ; make integer positive
+		MOV r2, #0x2D ;store ASCII '-' to add to string later
+I2S_START:
+		;ADD r1,r1,r2    ;Add num of digits to pointer r1=r1+r2
+		MOV r7,#0 		;Store 0 at r7 to be used as NULL
+		STRB r7,[r1]	;Store Null at adress pointed to by pointer
 		SUB r1,r1,#1	; Subtract 1 from pointer r1= r1-1
 DivideByTen:
-		MOV r3,#10		; Move 10 to r3 to be used for division and multiplication
-		UDIV r4,r0,r3		;r4 = q  Divide r0 by 10
-		MUL r5,r4,r3		;r5= p	 Multiply r4 by 10
-		SUB r6,r0,r5		;r6 = dig Subtract r5 from r0
-		ADD r7,r6,#0x30		;r7=ascii	add 0x30 to r7
-		STRB r7,[r1]		; store value of r7 at adress pointed to by pointer r1
-		MOV r0,r4			;Move value of r4 into r0
+		MOV r8,#10		; Move 10 to r8 to be used for division and multiplication
+		UDIV r9,r0,r8		;r9 = q  Divide r0 by 10
+		MUL r10,r9,r8		;r10 = p	 Multiply r9 by 10
+		SUB r10,r0,r10		;r10 = dig Subtract p from r0
+		ADD r10,r10,#0x30		;r7=ascii	add 0x30 to r10
+		STRB r10,[r1]		; store value of r10 at adress pointed to by pointer r1
+		MOV r0,r9			;Move value of r9 into r0
 		CMP r0,#0			; Compare value of r0 to 0
-		BEQ STOP			; We Branch if r0=0
-		SUB r1,r1,#1		; If r0!=0 then we subtarct 1 from r1 and branch
+		BEQ I2S_STOP		; We Branch if r0=0
+		SUB r1,r1,#1		; If r0!=0 then we subtract 1 from r1 and branch
 		B DivideByTen
-STOP:
+I2S_STOP:
+		CMP r2, #0x2D ;check to see if '-' still needs to be in string
+		BNE I2S_DONE
+		SUB r1, r1, #1 ; go to front of string
+		STRB r2, [r1] ;store '-' at front of string
+I2S_DONE:
 	LDMFD r13!, {r14}
 	MOV pc, lr
 
