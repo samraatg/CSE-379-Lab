3c6a7d10f3a9455463758c39596fd0a86f26f202
AviUB <47643721+AviUB@users.noreply.github.com>
2021-03-30 12:06:54 -0400

    gameplay complete
    fixed issues with walls moving after collision
    added game over message
    cleaned up handler routines and comments

--- lab5part4.s
+++ lab5part4.s
@@ -11,10 +11,11 @@ board: 	.string "------------", 0xA, 0xD
 		.string "|          |", 0xA, 0xD
 		.string "|          |", 0xA, 0xD
 		.string "------------", 0xA, 0xD, 0x0
-		; asterisks position range: 0 < 165 (0xA5)
+game_over:	.string "GAME OVER: You Collided With A Wall",0x0
+; asterisks position range: 0 < 165 (0xA5)
 ; can store exit flag, position, and direction in game_data
-; XXXE PSDR		bytes: [0,1]=direction [2-3]=position, [4]=flag
-game_data:	.space 4 ; placeholder for game's data in memory
+; XXXE PSDR		bytes: [0,1]=direction [2-3]=position, [4]=exitflag
+game_data:	.space 4 ; placeholder for game data in memory
 	.text
 	.global UART0_Handler
 	.global Switch_Handler
@@ -29,6 +30,7 @@ game_data:	.space 4 ; placeholder for game's data in memory
 	.global output_string
 ptr_to_board: .word board
 ptr_to_game_data: .word game_data
+ptr_to_game_over: .word game_over
 
 Left: .equ 0x0		; 00 = left
 Right: .equ 0x1		; 01 = right
@@ -48,20 +50,26 @@ lab5:
  	BL interrupt_init
  	BL timer_init
 
-	; push direction and initial coordinates to stack
-	MOV r10, #75 	; asterisks initial position
-	LSL r10, #8		; shift pos into correct bytes
+START:
+	; store initial game data
+	MOV r10, #75 			; asterisks initial pos
+	LSL r10, #8				; shift pos into correct bytes
 	ADD r10, r10, #Right	; set dir bits to right
 	; r10 = 0x0000 4B01
 	LDR r9, ptr_to_game_data
 	STR r10, [r9]
-LOOP:
 
+LOOP:
+	; check for exit flag
 	LDR r10, ptr_to_game_data
 	LDR r10, [r10]
 	AND r10, r10, #EXIT
 	CMP r10, #EXIT
 	BNE LOOP
+
+	; print game over statement on exit
+	LDR r0, ptr_to_game_over
+	BL output_string
 
  	LDMFD sp!, {r0-r12,lr}
  	MOV pc, lr
@@ -172,7 +180,7 @@ UART0_Handler:
 	LDR r10, ptr_to_game_data
 	LDR r9, [r10]
 
-	; load uart data
+	; load uart data (char input)
 	LDR r5, [r4]
 	CMP r5, #0x71 ; check if char is ASCII 'q'
 	BNE SPACE
@@ -195,10 +203,10 @@ Switch_Handler:
 	ORR r5, r5, #0x10 ; set bit 4 (SW1) to clear
 	STR r5, [r4, #0x41C]
 
-	; grab game data
+	; change game data
 	LDR r10, ptr_to_game_data
 	LDR r9, [r10]
-	EOR r9, r9, #0x3		; invert dir bits to change from ud<->lr
+	EOR r9, r9, #0x3		; invert dir bits to change from u/d <-> l/r
 	STR r9, [r10]
 
 	LDMFD sp!, {r0-r12,lr}
@@ -213,58 +221,69 @@ Timer_Handler:
  	ORR r5, r5, #0x1 ; set bit0 to 1 (TATOCINT)
  	STR r5, [r4, #0x024]
 
- 	; print board
- 	LDR r0, ptr_to_board
-	BL output_string
-
-	; load game_data into r10
+	; load game_data
 	LDR r10, ptr_to_game_data
-	LDR r10, [r10]
+	LDR r9, [r10]
 	; grab asterisks position and direction
-	UBFX r9, r10, #8, #8
-	AND r8, r10, #0xFF
+	UBFX r8, r9, #8, #8	; r8 = pos
+	AND r7, r9, #0xFF ; r7 = dir
 
 	; find position of character to swap with
-	CMP r8, #Up
+	; r6 = swap pos
+	CMP r7, #Up
 	BNE DOWN
-	SUB r7, r9, #RowLen ; move asterisks up 1 row
-	B SWAP
+	SUB r6, r8, #RowLen ; move asterisks up 1 row
+	B GET_CHARS
 DOWN:
-	CMP r8, #Down
+	CMP r7, #Down
 	BNE LEFT
-	ADD r7, r9, #RowLen ; move asterisks down 1 row
-	B SWAP
+	ADD r6, r8, #RowLen ; move asterisks down 1 row
+	B GET_CHARS
 LEFT:
-	CMP r8, #Left
+	CMP r7, #Left
 	BNE RIGHT
-	SUB r7, r9, #1 ; move asterisks left 1 column
-	B SWAP
+	SUB r6, r8, #1 ; move asterisks left 1 column
+	B GET_CHARS
 RIGHT:
-	CMP r8, #Right
-	BNE SWAP
-	ADD r7, r9, #1 ; move asterisks right 1 column
+	CMP r7, #Right
+	BNE GET_CHARS
+	ADD r6, r8, #1 ; move asterisks right 1 column
 
-SWAP:
-	; swap characters at indices pointed to by r7 and r9
+
+GET_CHARS:
+	; grab chars at asterisks pos and new pos from the board
 	LDR r0, ptr_to_board
-	LDRB r4, [r0, r7]
-	LDRB r5, [r0, r9]
-	STRB r4, [r0, r9]
-	STRB r5, [r0, r7]
+	LDRB r4, [r0, r6]	; r4 = char to swap with
+	LDRB r5, [r0, r8]	; r5 = asterisks
 
-	; store game data in memory
-	BFI r10, r7, #8, #8	; store new pos
+	; check for wall collision
 	CMP r4, #0x7C ; ASCII "|"
 	BEQ SET_EF
 	CMP r4, #0x2D ; ASCII "-"
 	BEQ SET_EF
-	B TH_EXIT
+	B SWAP
+
 SET_EF:
-	ORR r10, r10, #EXIT ; set exit flag if boundary is hit
+	; set exit flag if there was wall collision
+	ORR r9, r9, #EXIT
+	B TH_EXIT
+
+SWAP:
+	; store swapped chars in new position
+	STRB r4, [r0, r8]
+	STRB r5, [r0, r6]
+	; store new pos in game_data
+	BFI r9, r6, #8, #8
+
 TH_EXIT:
 	; store game_data in memory
-	LDR r9, ptr_to_game_data
-	STR r10, [r9]
+	STR r9, [r10]
+	; clear screen and print board
+	MOV r0, #0xC	; ASCII new page
+	BL output_character
+ 	LDR r0, ptr_to_board
+	BL output_string
+
 	LDMFD sp!, {r0-r12,lr}
 	BX lr
 
