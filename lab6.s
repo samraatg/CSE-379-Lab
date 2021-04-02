bc650d4db3cdf3048d370f5f4dfb6f15d455f4af
AviUB <47643721+AviUB@users.noreply.github.com>
2021-04-01 20:18:54 -0400

    cursor kinda works

--- lab6.s
+++ lab6.s
@@ -8,7 +8,7 @@ white_circle: .string 27,"[37mo",0
 yellow_circle: .string 27,"[33mo",0
 red_circle:	.string 27,"[31mo",0
 magenta_circle:	.string 27,"[35mo",0
-cursor: .string 27,"[8;14H",0
+cursor: .string 27,"[0;0H",0
 
 
 	.text
@@ -46,6 +46,8 @@ lab6:
 	BL interrupt_init
 
 	LDR r0, ptr_to_cursor
+	LDR r4, [r0] 		;top half of cursor (ESC [8;)
+	LDR r5, [r0, #4]	; bottom half of cursor (14H 0x0)
 	BL output_string
 
 ;print 10 X's
@@ -60,6 +62,8 @@ X_loop:
 
 	LDR r0, ptr_to_greenCircle
 	BL output_string
+LOOP:
+	B LOOP
 
 	LDMFD sp!, {r0-r12,lr}
  	MOV pc, lr
@@ -75,22 +79,62 @@ UART0_Handler:
 
 
 	; load uart data (char input)
-	LDR r5, [r4]
-	ldr r6,ptr_to_cursor
+	LDR r6, [r4]
+	LDR r5, ptr_to_cursor
+	LDR r7, [r5] 		;top half of cursor (ESC [8;)
+	LDR r8, [r5, #4]	; bottom half of cursor (14H 0x0)
+	; grab row and column of cursor
+	UBFX r9, r7, #16, #8	; r9 = line
+	UBFX r10, r8, #0, #16	; r10 = column
 UP:
-	CMP r5, #0x57 ; check if char is ASCII 'w'
+	CMP r6, #0x77 ; check if char is ASCII 'w'
 	BNE DOWN
+	ADD r9, r9, #1	; move line up
 DOWN:
-	CMP r5, #0x53 ; check if char is ASCII 'a'
+	CMP r6, #0x73 	; check if char is ASCII 's'
 	BNE LEFT
+	SUB r9, r9, #1	; move line down
 LEFT:
-	CMP r5, #0x41 ; check if char is ASCII 's'
+	CMP r6, #0x61 	; check if char is ASCII 'a'
 	BNE RIGHT
+	SUB r10, r10, #0x100	; move column left
 RIGHT:
-	CMP r5, #0x44 ; check if char is ASCII 'd'
+	CMP r6, #0x64 	; check if char is ASCII 'd'
 	BNE ELSE
+	ADD r10, r10, #0x100	; move column right
 ELSE:
+	; update postion
+	BFI r7, r9, #16, #8
+	BFI r8, r10, #0, #16
+	STR r7, [r5] 			; store top half of cursor (ESC [8;)
+	STR r8, [r5, #4]		; store bottom half of cursor (14H 0x0)
+	; output cursor
+	MOV r0, r5
+	BL output_string
 	LDMFD sp!, {r0-r12,lr}
 	BX lr
 
+Switch_Handler:
+	STMFD SP!,{r0-r12,lr}
+	; clear SW1 interrupt
+	MOV r4, #0x5000
+	MOVT r4, #0x4002 ; base address of GPIOF
+	LDR r5, [r4, #0x41C] ; load GPIOICR
+	ORR r5, r5, #0x10 ; set bit 4 (SW1) to clear
+	STR r5, [r4, #0x41C]
+
+	LDMFD sp!, {r0-r12,lr}
+	BX lr
+
+Timer_Handler:
+	STMFD SP!,{r0-r12,lr}
+	; clear timer interrupt
+	MOV r4, #0x0000
+ 	MOVT r4, #0x4003 ; T0 base address
+ 	LDR r5, [r4, #0x024] ; GPTMICR offset
+ 	ORR r5, r5, #0x1 ; set bit0 to 1 (TATOCINT)
+ 	STR r5, [r4, #0x024]
+
+	LDMFD sp!, {r0-r12,lr}
+	BX lr
 .end
